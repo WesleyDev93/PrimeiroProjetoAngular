a4cfa0881ca04b9b7d765340178fee35c3134a3c
WesleyDev93 <wesleyoliveiralimadasilva@gmail.com>
2023-09-12 18:19:12 -0300

    Subindo estrutura e cabeçalho do projeto

---
+++ src/app/cabecalho/meu-cabecalho/meu-cabecalho.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-cabecalho',
+  templateUrl: './meu-cabecalho.component.html',
+  styleUrls: ['./meu-cabecalho.component.css']
+})
+export class MeuCabecalhoComponent {
+
+}
