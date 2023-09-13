8b0960f11df0355bd033ea51466f7a9206fda472
WesleyDev93 <wesleyoliveiralimadasilva@gmail.com>
2023-09-13 14:54:10 -0300

    criando e estilizando o rodape

--- src/app/cabecalho/meu-cabecalho/meu-cabecalho.component.ts
+++ src/app/cabecalho/meu-cabecalho/meu-cabecalho.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class MeuCabecalhoComponent {
 
 
-  nome:string = 'Amorass';
+  nome:string = '';
   color :string = 'purple';
   constructor () {
 
--- src/app/principal/rodape/rodape.component.ts
+++ src/app/principal/rodape/rodape.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-rodape',
   templateUrl: './rodape.component.html',
-  styleUrls: ['./rodape.component.css']
+  styleUrls: ['./rodape.component.css'],
 })
 export class RodapeComponent {
 
+
+  nomeCla: string = 'nomedoclan';
+  Clans: string = 'outroclan';
+  CodClan: string ='QQQ000';
+  constructor () {
+
+  }
 }
