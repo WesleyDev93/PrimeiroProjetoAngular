2aefd46f6351c28162e8d9d93fe712f99420f97b
WesleyDev93 <wesleyoliveiralimadasilva@gmail.com>
2023-09-13 17:24:28 -0300

    aprimorando rodape  e cabecalho

--- src/app/cabecalho/meu-cabecalho/meu-cabecalho.component.ts
+++ src/app/cabecalho/meu-cabecalho/meu-cabecalho.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
+import { NumberValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-meu-cabecalho',
   templateUrl: './meu-cabecalho.component.html',
   styleUrls: ['./meu-cabecalho.component.css']
 })
-export class MeuCabecalhoComponent {
 
+export class MeuCabecalhoComponent  {
 
-  nome:string = '';
+  // Nome e cores dinamicos
+  nome:string = 'nome';
   color :string = 'purple';
-  constructor () {
 
-  }
+
+
+
+  constructor () {}
+
 }
+
+
+
+
